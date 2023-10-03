6d7471d48a5c09cea95f6139b9c77cde2504a398
VCarames <109115310+VCarames@users.noreply.github.com>
2023-10-02 19:22:07 -0700

    Features Component + JSON File
    "Features" JSON file and component created and added to "Home" page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Clients from "@/components/home/Clients";
 import Benefits from "@/components/home/Benefits";
 import Hero from "@/components/home/Hero";
+import Features from "@/components/home/Features";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Hero />
       <Clients />
       <Benefits />
+      <Features />
     </>
   );
 }
---
+++ src/components/home/Features.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import featuresData from "/public/data/features";
+
+function Features() {
+  return (
+    <section className="features-section">
+      <div className="features-section__content">
+        <h2 className="features-section__heading visually-hidden">Features</h2>
+        <ul className="features-section__list" role="list">
+          {featuresData.features.map((feature) => (
+            <li
+              className="features-section__list-item"
+              key={feature.id}
+              role="listitem"
+            >
+              <Image
+                className="features-section__list-icon"
+                src={feature.icon}
+                alt=""
+                width={106}
+                height={106}
+              />
+              <div className="features-section__list-container">
+                <h3 className="features-section__list-heading primary__subheading">
+                  {feature.heading}
+                </h3>
+                <p className="features-section__list-text secondary__text">
+                  {feature.text}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
