3d4e1ba058febe67322ad8bafcec553f407b8d84
VCarames <109115310+VCarames@users.noreply.github.com>
2023-10-02 21:25:02 -0700

    "CTA Section" Component - Styling
    Initial styling for the "CTA Section" component.

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -8,7 +8,13 @@ function CTASection() {
           Ready to start?
         </h2>
 
-        <CTA />
+        <CTA
+          formClassName="cta-form cta-form--section"
+          inputClassName="cta-form__email cta-form__email--section"
+          labelClassName="cta-form__email-label visually-hidden"
+          buttonClassName="cta-form__button cta-form__button--section button button--primary"
+          spanClassName=""
+        />
       </div>
     </section>
   );
