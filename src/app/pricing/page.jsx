8adcc123bd21fe5f1e4d7d94036e267ace563002
VCarames <109115310+VCarames@users.noreply.github.com>
2023-10-03 14:38:06 -0700

    "CTA Section"
    "CTA Section" component added to "Pricing" page

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,9 +1,11 @@
+import CTASection from "@/components/CTASection";
 import Plans from "@/components/pricing/Plans";
 
 function Pricing() {
   return (
     <>
       <Plans />
+      <CTASection />
     </>
   );
 }
