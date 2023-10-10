2a5856bea1ef6cf12ee1d6a6cb4872462f616ee2
VCarames <109115310+VCarames@users.noreply.github.com>
2023-10-09 21:52:01 -0700

    Form Validation
    Form validation functionality added to the name, email, company name, title and message.
    
    If an invalid email is enter a message appears.
    
    If any of the inputs are left blank when the users tries to submit the form, an error message will be displayed.

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,6 +1,72 @@
+"use client";
+
+import React, { useState } from "react";
+
 function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    companyName: "",
+    title: "",
+    message: "",
+    consent: false,
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    companyName: "",
+    title: "",
+    message: "",
+  });
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newErrors = {};
+
+    if (formData.name.trim() === "") {
+      newErrors.name = "Name cannot be empty";
+    }
+
+    if (!validateEmail(formData.email)) {
+      newErrors.email = "Email is not valid";
+    }
+
+    if (formData.companyName.trim() === "") {
+      newErrors.companyName = "Company Name cannot be empty";
+    }
+
+    if (formData.title.trim() === "") {
+      newErrors.title = "Title cannot be empty";
+    }
+
+    if (formData.message.trim() === "") {
+      newErrors.message = "Message cannot be empty";
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+    }
+  };
+
   return (
-    <form className="contact-section__form">
+    <form className="contact-section__form" onSubmit={handleSubmit} noValidate>
       <input
         type="text"
         name="name"
@@ -8,10 +74,13 @@ function Form() {
         className="contact-section__form--name"
         placeholder="Name"
         pattern="[a-zA-Z]*"
+        value={formData.name}
+        onChange={handleInputChange}
       />
       <label className="visually-hidden" htmlFor="name">
         Name
       </label>
+      <span className="error">{errors.name}</span>
 
       <input
         type="email"
@@ -19,10 +88,13 @@ function Form() {
         id="email"
         className="contact-section__form--email"
         placeholder="Email Address"
+        value={formData.email}
+        onChange={handleInputChange}
       />
       <label className="visually-hidden" htmlFor="email">
         Email
       </label>
+      <span className="error">{errors.email}</span>
 
       <input
         type="text"
@@ -35,6 +107,7 @@ function Form() {
       <label className="visually-hidden" htmlFor="company">
         Company Name
       </label>
+      <span className="error">{errors.companyName}</span>
 
       <input
         type="text"
@@ -47,6 +120,7 @@ function Form() {
       <label className="visually-hidden" htmlFor="title">
         Title
       </label>
+      <span className="error">{errors.title}</span>
 
       <input
         type="text"
@@ -58,21 +132,27 @@ function Form() {
       <label className="visually-hidden" htmlFor="message">
         Message
       </label>
+      <span className="error">{errors.message}</span>
 
       <div className="contact-section__form-container">
         <input
           type="checkbox"
-          name=""
-          id=""
+          name="consent"
+          id="consent"
           className="contact-section__form--checkbox"
           aria-describedby="updates"
+          checked={formData.consent}
+          onChange={handleInputChange}
         />
         <p id="updates" className="contact-section__form--consent">
           Stay up-to-date with company announcements and updates to our API
         </p>
       </div>
 
-      <button className="contact-section__form-button button button--secondary">
+      <button
+        className="contact-section__form-button button button--secondary"
+        type="submit"
+      >
         Submit
       </button>
     </form>
